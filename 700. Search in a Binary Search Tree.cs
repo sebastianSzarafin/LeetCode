707a252905dee8accd35a5db0a6529fc795ea2e0
sebastianSzarafin <74315304+sebastianSzarafin@users.noreply.github.com>
2022-09-29 12:42:02 +0200

    Create 700. Search in a Binary Search Tree.cs

---
+++ 700. Search in a Binary Search Tree.cs
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/search-in-a-binary-search-tree/?envType=study-plan&id=data-structure-i
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+        if(root == null) return null;
+        if(root.val == val) return root;
+        if(root.val > val) return SearchBST(root.left, val);
+        else return SearchBST(root.right, val);
+    }
+}
